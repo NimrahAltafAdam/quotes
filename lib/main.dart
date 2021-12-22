b60a750dfae3ba180db9bbf5799d905b914aee05
NimrahAltafAdam <nimrahadam.123@gmail.com>
2021-12-22 08:55:36 -0800

    Custom Classes
    Created a custom class to list the authors and their quotes together one after another.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,11 +15,13 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-    'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genuis',
-    'The truth is rarely pure and never simple'
+  List<Quote> quotes = [
+    Quote(text: 'Be yourself; everyone else is already taken', author: 'Osca Wilde'),
+    Quote(text: 'I have nothing to declare except my genuis', author: 'Osca Wilde'),
+    Quote(text: 'The truth is rarely pure and never simple', author: 'Osca Wilde'),
+
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +32,8 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
+        //ALWAYS USE CURLY BRACKETS WHEN YOU WISH TO DEFINE OR DISPLAY THE PROPERTIES OF VARIABLES
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       )
     );
   }
